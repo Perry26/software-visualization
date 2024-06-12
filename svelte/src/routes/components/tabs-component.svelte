940252643908ef15cbba81c8dfc1cc9e7ab8f9fb
perry26 <perry_kloet@live.nl>
2024-06-12 12:35:20 +0200

    Bugfix tabs component

--- svelte/src/routes/components/tabs-component.svelte
+++ svelte/src/routes/components/tabs-component.svelte
@@ -5,7 +5,7 @@
 
 	const firstClass = 'rounded-s-lg';
 	const selectedClass =
-		'text-gray-900 bg-gray-100 active focus:outline-none dark:bg-gray-700 dark:text-white';
+		'text-gray-900 bg-gray-100 active focus:outline-none dark:bg-gray-700 dark:text-white cursor-default';
 	const notSelectedClass =
 		'bg-white hover:text-gray-700 hover:bg-gray-50 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700';
 	const defaultClasses =
@@ -21,8 +21,7 @@
 >
 	{#each tabs as tab, i}
 		<li class="w-full focus-within:z-10">
-			<a
-				href="#"
+			<button
 				on:click={() => {
 					//@ts-ignore
 					sidePanelTab = SidePanelTab[tab];
@@ -35,59 +34,8 @@
 									sidePanelTab === SidePanelTab[tab] ? selectedClass : notSelectedClass
 								}
                 `}
-				aria-current="page">{tab}</a
+				aria-current="page">{tab}</button
 			>
 		</li>
 	{/each}
-
-	<!-- <li class="w-full focus-within:z-10">
-		<a
-			href="#"
-			on:click={() => {
-				sidePanelTab = SidePanelTab.Input;
-			}}
-			class="inline-block w-full p-4 text-gray-900 bg-gray-100 border-r border-gray-200 dark:border-gray-700 rounded-s-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white"
-			aria-current="page">Input</a
-		>
-	</li>
-	<li class="w-full focus-within:z-10">
-		<a
-			href="#"
-			on:click={() => {
-				sidePanelTab = SidePanelTab.Config;
-			}}
-			class="inline-block w-full p-4 bg-white border-r border-gray-200 dark:border-gray-700 hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-			>Config</a
-		>
-	</li>
-	<li class="w-full focus-within:z-10">
-		<a
-			href="#"
-			on:click={() => {
-				sidePanelTab = SidePanelTab.DrawSettings;
-			}}
-			class="inline-block w-full p-4 bg-white border-r border-gray-200 dark:border-gray-700 hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-			>DrawSettings</a
-		>
-	</li>
-	<li class="w-full focus-within:z-10">
-		<a
-			href="#"
-			on:click={() => {
-				sidePanelTab = SidePanelTab.Layout;
-			}}
-			class="inline-block w-full p-4 bg-white border-r border-gray-200 dark:border-gray-700 hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-			>Layout</a
-		>
-	</li>
-	<li class="w-full focus-within:z-10">
-		<a
-			href="#"
-			on:click={() => {
-				sidePanelTab = SidePanelTab.Evaluation;
-			}}
-			class="inline-block w-full p-4 bg-white border-s-0 border-gray-200 dark:border-gray-700 rounded-e-lg hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-			>Evaluation</a
-		>
-	</li> -->
 </ul>
