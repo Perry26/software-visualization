beba38545897cb06aae04a7b5b4a1d99e5acd308
perry26 <perry_kloet@live.nl>
2024-07-25 13:49:22 +0200

    Hardcode evaluation data to allow deployment

--- svelte/src/routes/explore-layouts/+page.server.ts
+++ svelte/src/routes/explore-layouts/+page.server.ts
@@ -1,33 +1,50 @@
 import type {DrawSettingsInterface} from '$types';
-import {readFileSync, readdirSync} from 'fs';
+//import {readFileSync, readdirSync, writeFileSync} from 'fs';
+import data from './data.txt?raw';
+
+const localMode = false;
+const serverMode = true;
+const writeMode = false;
 
 export function load(_) {
-	const string = readFileSync('../python/output-data/evaluationResults.csv').toString();
-	// Parse the csv-data.
-	// While we're at it, we also convert stuff to a number if possible
-	const evaluationResults = string
-		.split('\n')
-		.map(s => s.split(';').map((s, i) => (i < 10 ? Number(s) : s)));
+	if (localMode) {
+		/*
+		const string = readFileSync('../python/output-data/evaluationResults.csv').toString();
+		// Parse the csv-data.
+		// While we're at it, we also convert stuff to a number if possible
+		const evaluationResults = string
+			.split('\n')
+			.map(s => s.split(';').map((s, i) => (i < 10 ? Number(s) : s)));
 
-	// Now the JSON data
-	const jsonObject: {[hash: string]: DrawSettingsInterface} = {};
-	readdirSync('../python/output-data/json').forEach(f => {
-		const hash = f.replace('.json', '');
-		const t = readFileSync('../python/output-data/json/' + f).toString();
-		const obj = JSON.parse(t.split(';')[0]);
-		jsonObject[hash] = obj;
-	});
+		// Now the JSON data
+		const jsonObject: {[hash: string]: DrawSettingsInterface} = {};
+		readdirSync('../python/output-data/json').forEach(f => {
+			const hash = f.replace('.json', '');
+			const t = readFileSync('../python/output-data/json/' + f).toString();
+			const obj = JSON.parse(t.split(';')[0]);
+			jsonObject[hash] = obj;
+		});
 
-	// Let's also keep the headers, for ease of use
-	// The final column is the hash value, and is unlabelled.
-	const header = string
-		.split('\n')[0]
-		.split(';')
-		.map(d => d.replace(/['"]+/g, ''));
+		// Let's also keep the headers, for ease of use
+		// The final column is the hash value, and is unlabelled.
+		const header = string
+			.split('\n')[0]
+			.split(';')
+			.map(d => d.replace(/['"]+/g, ''));
 
-	return {
-		evaluationResults: evaluationResults.filter(e => e.length === 11),
-		header,
-		jsonData: jsonObject,
-	};
+		const result = {
+			evaluationResults: evaluationResults.filter(e => e.length === 11),
+			header,
+			jsonData: jsonObject,
+		};
+
+		if (writeMode) {
+			writeFileSync('./src/routes/explore-layouts/data.txt', JSON.stringify(result));
+		}
+
+		return result;
+		*/
+	} else if (serverMode) {
+		return JSON.parse(data);
+	}
 }
--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -12,4 +12,5 @@ export default defineConfig({
 			$types: '/src/types',
 		},
 	},
+	assetsInclude: ['**/*.txt'],
 });
